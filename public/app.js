492e0ed6a233175d920bf286d5c667701c5eab6c
Paul Clatworthy <strayllama@gmail.com>
2018-04-17 09:31:15 +0100

    something

--- public/app.js
+++ public/app.js
@@ -1,13 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  const catImageContainer = document.querySelector('#cats'); // create container where image is to go
-
+  const catImageContainer = document.querySelector('#cats'); // create container where ul is to go
   imageContainer.innerHTML = '';  // clear it out
 
-  const image = document.createElement('img'); // create img element
 
-  image.src = 'images/IMG1.jpg'; // add the source of image to element
+  const createCat = function (src, container, catName, catFood) {
+    const catUL = document.createElement('ul');
+    const catLiName = document.createElement('li');
+    const catLiFavFood = document.createElement('li');
+    const catImage = document.createElement('img');
 
-  container.appendChild(image); // add the img element to the image container
+    catLiName.textContent = `Name: ${catName}`;
+    catLiFavFood.textContent = `Favourite food: ${catFood}`;
+    image.src = src; // add the source of image to element
+    catUL.appendChild(catLiName);
+    catUL.appendChild(catLiFavFood);
+    catUL.appendChild(catImage);
+    container.appendChild(catUL);
+  };
+
+
+
+
+
+for (let i = 1; i < 4 ; i++) {
+//  createImage(`images/`)
+};
+
 
 });
